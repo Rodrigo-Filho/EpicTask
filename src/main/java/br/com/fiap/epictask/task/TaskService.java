d65571587656e3d3f627a8b279d4af8e42360e1a
RodrigoFiapp <rodrigogoncalves2005t@gmail.com>
2024-09-26 15:37:21 -0300

    consulta de tarefas

---
+++ src/main/java/br/com/fiap/epictask/task/TaskService.java
@@ -0,0 +1,16 @@
+package br.com.fiap.epictask.task;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TaskService {
+    private final TaskRepository taskRepository;
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+    public List<Task> findAll(){
+        return taskRepository.findAll();
+    }
+}
